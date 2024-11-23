6d537242a96274ea6eb35cd5b61737d1aa86a534
Kushal <kushalsharmahacker678@gmail.com>
2024-11-23 13:29:55 +0530

    HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import Logo from '../assets/LogoWName.svg';
-import Navbar from '../components/HomePage/Navbar.';
+import Navbar from '../components/HomePage/Navbar';
+import IntroSec from '../components/HomePage/IntroSec';
+import { useScrollPosition } from '../hooks/useScroll';
+import LogoWName from '../assets/LogoWName.svg'
 
 const HomePage = () => {
+    const scrollPosition = useScrollPosition();
     return (
-        <div className='p-4 w-screen min-h-screen'>
-            <img src={Logo} alt="" width={100} />
-            <h1 className='text-l font-thin'>Effortless Room Booking for All</h1>
+        <div className='min-w-screen min-h-screen overflow-hidden '>
+            <div className={`header flex items-center justify-between rounded-b-xl px-10 py-3 fixed z-20 w-full overflow-hidden top-0 transition-all duration-500 ${scrollPosition > 0 ? 'bg-[#29171cd0] shadow-lg' : 'bg-transparent'}`} >
+                <div className='text-center flex items-center'><img src={LogoWName} alt="Logo" className='md:w-[7%]' /></div>
+                <Navbar />
+            </div>
+            <div className='relative'>
+                <IntroSec />
+            </div>
+            <div>
+                hi
+            </div>
         </div>
     )
 }
